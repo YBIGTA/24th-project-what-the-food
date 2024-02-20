ccfa8bcb11a26d06652486704060fe049e500af7
wjddid000624 <wjddid000624@yonsei.ac.kr>
2024-02-20 13:29:17 +0900

    Update detection.py
    Display input image to user.

--- detection.py
+++ detection.py
@@ -1,17 +1,27 @@
 from ultralytics import YOLO
 from roboflow import Roboflow
+from PIL import Image
 
 food_list = ['된장찌개', '어묵볶음', '잔치국수', '제육볶음', '김치찜', '콩나물국', '콩나물무침', '쌀밥', '순두부찌개', '육개장']
 
+# display image
+def display_image(image_path, width=100):
+    img = Image.open(image_path)
+
+    img.show()
+
+# detect food from image
 def detection(image_path):
+    # test image
+    test_image = image_path
+
+    display_image(test_image)
+
     # load model from roboflow
     rf = Roboflow(api_key="On22bsh1BDNtkVfYZhG9")
     project = rf.workspace("ybigta24thproject").project("what-the-food-9wdgr")
 
     model = project.version(4).model
-
-    # test image
-    test_image = image_path
 
     # detect food
     pred = model.predict(test_image, confidence=30, overlap=30).json()
