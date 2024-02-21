12eb58774d6f505a1f6a3c4e579a23e4ab1fd4d1
wjddid000624 <wjddid000624@yonsei.ac.kr>
2024-02-21 21:20:17 +0900

    Update detection.py

--- detection.py
+++ detection.py
@@ -1,40 +1,37 @@
+import json
 from ultralytics import YOLO
-from roboflow import Roboflow
 from PIL import Image
 
+weight_path = 'Object_detection\YOLO_weights\\best.pt'
 food_list = ['된장찌개', '어묵볶음', '잔치국수', '제육볶음', '김치찜', '콩나물국', '콩나물무침', '쌀밥', '순두부찌개', '육개장']
 
-# display image
 def display_image(image_path, width=100):
     img = Image.open(image_path)
 
     img.show()
 
-# detect food from image
 def detection(image_path):
     # test image
     test_image = image_path
-
     display_image(test_image)
 
-    # load model from roboflow
-    rf = Roboflow(api_key="YOUR_API_KEY")
-    project = rf.workspace("ybigta24thproject").project("what-the-food-9wdgr")
-
-    model = project.version(4).model
+    # load food
+    model = YOLO(weight_path)
 
     # detect food
-    pred = model.predict(test_image, confidence=30, overlap=30).json()
+    pred = model.predict(test_image, conf = 0.3, verbose=False)
+    names = model.names
 
     # make list of detected food
     detected_food = []
-    for i in pred['predictions']:
-        detected_food.append( food_list[i['class_id']] )
-
+    for r in pred:
+        for c in r.boxes.cls:
+            detected_food.append(food_list[int(c)])
+    
     return detected_food
 
 if __name__ == "__main__":
     print("submit image file name")
     file_name = input()
     detected_food = detection(file_name)
-    print(detected_food)
+    print("detected_food:", detected_food)
